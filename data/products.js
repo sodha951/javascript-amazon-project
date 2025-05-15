9a5ed3305775e16e59e927c4df5f7e3fd2332c0f
Alpesh-261 <alpeshsodha2604@gmail.com>
2025-05-15 12:43:53 +0530

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -59,6 +59,36 @@ class Clothing extends Product{
   }
 }
 
+/*
+const date = new Date();
+console.log(date.toLocaleTimeString());
+*/
+
+/*
+console.log(this);
+
+const object2 = {
+  a : 2,
+  b : this.a
+};
+*/
+
+/*
+function logThis () {
+  console.log(this);
+}
+logThis();
+logThis.call('hello');
+
+this
+const object3 = {
+  method: () => {
+    console.log(this);
+    
+  }
+}
+*/
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
